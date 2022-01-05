09c63b629afa9f90f6d737bec11208ea92f22a31
Spawn4real <m.demchenko2001@gmail.com>
2022-01-05 23:53:11 +0700

    Lesson_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,15 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 
+main_menu = [
+    {'href': '/', 'name': 'Домой'},
+    {'href': 'products', 'name': 'Продукты'},
+    {'href': 'contact', 'name': 'Контакты'},
+]
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+    context = {
+        'title': title,
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/index.html', context=context,)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/products.html', context=context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = 'Контакты'
+    context = {
+        'title': title,
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/contact.html', context=context)
 
